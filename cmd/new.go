ba868036293c98974de3612851853b64f36be899
Rafael Santos <15151000+blackmagicbox@users.noreply.github.com>
2026-03-31 23:00:21 +0200

    Add 'new' command to create a VM

--- cmd/new.go
+++ cmd/new.go
@@ -1,1 +1,20 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Create a new VM",
+	Long:  "Create a new vm from an existing image or from a new one selected by the user",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Creating a new vm...")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(newCmd)
+}
