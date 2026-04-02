e2050fd36a6247f415ca6445534f431f9158183a
Rafael Santos <15151000+blackmagicbox@users.noreply.github.com>
2026-04-02 17:30:14 +0200

    FEAT start the TUI wizzard on the 'new' command

--- cmd/new.go
+++ cmd/new.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/blackmagicbox/labctl/internal/tui"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +14,16 @@ var newCmd = &cobra.Command{
 	Short: "Create a new VM",
 	Long:  "Create a new vm from an existing image or from a new one selected by the user",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Creating a new vm...")
+		m := tui.New("rocky-9-20260331")
+		p := tea.NewProgram(m)
+
+		result, err := p.Run()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error %v\n", err)
+		}
+		finalModel := result.(tui.Model)
+		fmt.Printf("VM name: %s\n", finalModel.Value())
+
 	},
 }
 
