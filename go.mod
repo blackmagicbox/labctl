f526808f594efa6a45c15415b56c8e58632b9abb
Rafael Santos <15151000+blackmagicbox@users.noreply.github.com>
2026-04-21 19:45:24 +0200

    Feat Vm selection and base config fields to the model.[DRAFT] (fix #4) (#5)
    * Feat Vm selection and base config fields to the model.[DRAFT]
    
    * FEAT add image selection functionality to TUI wizard
    
    * FEAT add VM name input field and update flow in TUI
    
    * Feat Hostname input field [DRAFT]
    
    * Feat Progressive summary\Dynamic date\Default values for VMName and Hostname
    
    * Feat basic vm setup TUI
    
    * Feat basic vm setup TUI

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/blackmagicbox/labctl
 go 1.26.1
 
 require (
+	charm.land/bubbles/v2 v2.1.0
 	charm.land/bubbletea/v2 v2.0.2
 	charm.land/lipgloss/v2 v2.0.2
 	github.com/spf13/cobra v1.10.2
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/charmbracelet/colorprofile v0.4.2 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20260205113103-524a6607adb8 // indirect
 	github.com/charmbracelet/x/ansi v0.11.6 // indirect
@@ -19,7 +21,7 @@ require (
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
-	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.21 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,30 +1,91 @@
 package tui
 
 import (
+	"fmt"
+	"time"
+
+	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 )
 
 type step int
 
 const (
-	stepDistro step = iota
+	stepPathToImg step = iota
+	stepExistentImg
+	stepDistro
+	stepImage
+	stepVMName
+	stepHostname
+	stepUsername
+	stepDisk
+	stepMemory
+	stepCPU
 )
 
 type Model struct {
-	step           step
-	Distro         selectModel
-	SelectedDistro string
+	step     step
+	Distro   selectModel
+	Image    selectModel
+	VMName   textinput.Model
+	Hostname textinput.Model
+	Username textinput.Model
+	Disk     textinput.Model
+	Memory   textinput.Model
+	CPU      textinput.Model
+}
+
+var Images = map[string][]string{
+	"Debian/Ubuntu": {"ubuntu-noble24", "ubuntu-1604", "ubuntu-1804"},
+	"Rhel/Fedora":   {"centos-stream-10", "fedora", "rocky-10"},
+	"Arch/Manjaro":  {"steamOS", "arch-linux-basic"},
 }
 
 func New() Model {
+	// VM name
+	n := textinput.New()
+	n.CharLimit = 156
+	n.SetWidth(156)
+	// Hostname
+	h := textinput.New()
+	h.Placeholder = "linux-lab"
+	h.CharLimit = 156
+	h.SetWidth(156)
+	// User name
+	u := textinput.New()
+	u.Placeholder = "admin"
+	u.CharLimit = 156
+	u.SetWidth(156)
+	// Disk Size
+	d := textinput.New()
+	d.Placeholder = "20G"
+	d.CharLimit = 7
+	d.SetWidth(20)
+	// Memory
+	m := textinput.New()
+	m.Placeholder = "2048MB"
+	m.CharLimit = 7
+	m.SetWidth(20)
+	// CPUs
+	c := textinput.New()
+	c.Placeholder = "2"
+	c.CharLimit = 3
+	c.SetWidth(20)
+
 	return Model{
-		step:   stepDistro,
-		Distro: newSelect("distro", []string{"Debian/Ubuntu", "Rhel/Fedora", "Arch/Manjaro"}),
+		step:     stepDistro,
+		Distro:   newSelect("distro", []string{"Debian/Ubuntu", "Rhel/Fedora", "Arch/Manjaro"}),
+		VMName:   n,
+		Hostname: h,
+		Username: u,
+		Disk:     d,
+		Memory:   m,
+		CPU:      c,
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -34,19 +95,147 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		default:
-			m.Distro, _ = m.Distro.Update(msg)
-			m.SelectedDistro = m.Distro.Value()
+			switch m.step {
+			case stepDistro:
+				m.Distro, _ = m.Distro.Update(msg)
+				if m.Distro.Chosen() {
+					m.Image = newSelect("image", Images[m.Distro.Value()])
+					m.step = stepImage
+				}
+			case stepImage:
+				m.Image, _ = m.Image.Update(msg)
+				if m.Image.Chosen() {
+					m.VMName.Placeholder = fmt.Sprintf("%s-%s", m.Image.Value(), time.Now().Format("20060102"))
+					m.VMName.Focus()
+					m.step = stepVMName
+				}
+			case stepVMName:
+				m.VMName, _ = m.VMName.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.VMName.Blur()
+					if m.VMName.Value() == "" {
+						m.VMName.SetValue(m.VMName.Placeholder)
+					}
+					m.Hostname.Focus()
+					m.step = stepHostname
+				}
+			case stepHostname:
+				m.Hostname, _ = m.Hostname.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.Hostname.Blur()
+					if m.Hostname.Value() == "" {
+						m.Hostname.SetValue(m.Hostname.Placeholder)
+					}
+					m.Username.Focus()
+					m.step = stepUsername
+				}
+			case stepUsername:
+				m.Username, _ = m.Username.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.Username.Blur()
+					if m.Username.Value() == "" {
+						m.Username.SetValue(m.Username.Placeholder)
+					}
+					m.Disk.Focus()
+					m.step = stepDisk
+				}
+			case stepDisk:
+				m.Disk, _ = m.Disk.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.Disk.Blur()
+					if m.Disk.Value() == "" {
+						m.Disk.SetValue(m.Disk.Placeholder)
+					}
+					m.Memory.Focus()
+					m.step = stepMemory
+				}
+			case stepMemory:
+				m.Memory, _ = m.Memory.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.Memory.Blur()
+					if m.Memory.Value() == "" {
+						m.Memory.SetValue(m.Memory.Placeholder)
+					}
+					m.CPU.Focus()
+					m.step = stepCPU
+				}
+			case stepCPU:
+				m.CPU, _ = m.CPU.Update(msg)
+				if msg.Code == tea.KeyEnter {
+					m.CPU.Blur()
+					if m.CPU.Value() == "" {
+						m.CPU.SetValue(m.CPU.Placeholder)
+					}
+				}
+				m.step++
+			default:
+			}
 		}
+
 	}
 	var cmd tea.Cmd
 	return m, cmd
-
 }
 
 func (m Model) View() tea.View {
-	return m.Distro.View()
+	out := ""
+	if m.Distro.Chosen() {
+		out = fmt.Sprintf("%s✔ distro: %s\n", out, m.Distro.Value())
+	}
+	if m.Image.Chosen() {
+		out = fmt.Sprintf("%s✔ image: %s\n", out, m.Image.Value())
+	}
+	if m.VMName.Value() != "" && !m.VMName.Focused() {
+		out = fmt.Sprintf("%s✔ VM name: %s\n", out, m.VMName.View())
+	}
+	if m.Hostname.Value() != "" && !m.Hostname.Focused() {
+		out = fmt.Sprintf("%s✔ hostname: %s\n", out, m.Hostname.View())
+	}
+	if m.Username.Value() != "" && !m.Username.Focused() {
+		out = fmt.Sprintf("%s✔ username: %s\n", out, m.Username.View())
+	}
+	if m.Disk.Value() != "" && !m.Disk.Focused() {
+		out = fmt.Sprintf("%s✔ disk: %s\n", out, m.Disk.View())
+	}
+	if m.Memory.Value() != "" && !m.Memory.Focused() {
+		out = fmt.Sprintf("%s✔ memory: %s\n", out, m.Memory.View())
+	}
+	if m.CPU.Value() != "" && !m.CPU.Focused() {
+		out = fmt.Sprintf("%s✔ CPU: %s\n", out, m.CPU.View())
+	}
+
+	switch m.step {
+	case stepDistro:
+		return m.Distro.View()
+	case stepImage:
+		return tea.NewView(fmt.Sprintf("%s%v", out, m.Image.View().Content))
+	case stepVMName:
+		return tea.NewView(fmt.Sprintf("%s? VM Name: %s", out, m.VMName.View()))
+	case stepHostname:
+		return tea.NewView(fmt.Sprintf("%s? hostname: %s", out, m.Hostname.View()))
+	case stepUsername:
+		return tea.NewView(fmt.Sprintf("%s? username: %s", out, m.Username.View()))
+	case stepDisk:
+		return tea.NewView(fmt.Sprintf("%s? disk: %s", out, m.Disk.View()))
+	case stepMemory:
+		return tea.NewView(fmt.Sprintf("%s? memory: %s", out, m.Memory.View()))
+	case stepCPU:
+		return tea.NewView(fmt.Sprintf("%s? CPU: %s", out, m.CPU.View()))
+	default:
+		return tea.NewView(out)
+	}
 }
 
-func (m Model) Value() string {
-	return m.SelectedDistro
+func (m Model) Value() []string {
+	// Return a String with all the settings
+	return []string{
+		m.VMName.View(),
+		m.Hostname.View(),
+		m.Username.View(),
+		m.Disk.View(),
+		m.Memory.View(),
+		m.CPU.View(),
+		m.Distro.Value(),
+		m.Image.Value(),
+	}
 }
