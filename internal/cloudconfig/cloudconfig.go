b653aaf1d3f3d915b62733c2e41d4d43c7141bc2
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2026-05-18 17:49:54 +0200

    feat Config generation [DRAFT]

---
+++ internal/cloudconfig/cloudconfig.go
@@ -0,0 +1,15 @@
+package cloudconfig
+
+import "github.com/blackmagicbox/labctl/internal/vm"
+
+func Generate(cfg *vm.Config) (string, error) {
+	return "", nil
+}
+
+func outputDir(vmName string) (string, error) {
+	return "", nil
+}
+
+func renderTemplate(name string, cfg vm.Config) ([]byte, error) {
+	return []byte{}, nil
+}
